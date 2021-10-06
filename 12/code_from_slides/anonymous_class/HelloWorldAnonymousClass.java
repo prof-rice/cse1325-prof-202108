172801cc356e1c9e31b95abe8ddd28bf19433100
Professor Rice <george.rice@uta.edu>
2021-10-06 18:23:26 -0500

    12 code

---
+++ 12/code_from_slides/anonymous_class/HelloWorldAnonymousClass.java
@@ -0,0 +1,20 @@
+interface HelloWorld {
+    public void greetSomeone(String someone);
+}
+
+public class HelloWorldAnonymousClass {
+    public void sayHello() {
+        HelloWorld englishGreeting = new HelloWorld() {
+            String name = "world";
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hello " + name);
+            }
+        };
+        englishGreeting.greetSomeone("Professor Rice");
+    }
+    
+    public static void main(String[] args) {
+        (new HelloWorldAnonymousClass()).sayHello();
+    }
+}
